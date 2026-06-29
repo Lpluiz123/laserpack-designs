ca1e759a5c4c75deca5adfe0578cf542126c8409
Lpluiz123 <luizfellipe.ilha@gmail.com>
2026-06-29 17:57:58 -0300

    fix: aceitar certificado ssl do supabase

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -105,10 +105,12 @@ app.listen(3000, () => {
 import { Client } from 'pg';
 
 async function testarConexao() {
-  // Usamos a string completa com o pgbouncer ativado
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL
-  });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
+});
   
   try {
     await client.connect();
